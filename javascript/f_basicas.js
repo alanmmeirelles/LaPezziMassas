7e86b9e91261cdcd5d791be17705340d19fc7ff1
Alan Martins Meirelles <50336705+alanmmeirelles@users.noreply.github.com>
2019-06-25 16:30:55 -0300

    update animation menu

--- javascript/f_basicas.js
+++ javascript/f_basicas.js
@@ -1,15 +1,20 @@
-function func_menu(){
+function func_menu() {
       var abrir = document.querySelector('ul#Menu_abre_fecha')
+      var girar = document.querySelector('img.icone_m')
 
-      if (abrir.style.opacity == 1){
+      if (abrir.style.opacity == 1) {
         abrir.style.opacity = '0'
-        abrir.style.top = '-35rem'
-        abrir.style.transition = 'opacity 0.5s, top 1s'   
+        abrir.style.top = '-24rem'
+        abrir.style.transition = 'opacity 0.5s, top 1s'
+        girar.style.transform = 'rotate(0deg)'
+        girar.style.transition = 'transform 0.7s'
       }
-      else{
+      else {
         abrir.style.opacity = '1'
         abrir.style.top = '3.2rem'
-        abrir.style.transition = 'opacity 0.5s, top 1s'    
+        abrir.style.transition = 'opacity 0.5s, top 1s'
+        girar.style.transform = 'rotate(360deg)'
+        girar.style.transition = 'transform 0.7s'
       }
 }
 
